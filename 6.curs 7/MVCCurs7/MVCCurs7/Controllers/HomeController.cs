fb6575d93f5ccb90de1d7a4a6f5cb03e73f31a62
johnbogdan <ionut_moldo94@yahoo.com>
2017-01-22 17:42:36 +0200

    Curs 7

---
+++ 6.curs 7/MVCCurs7/MVCCurs7/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCCurs7.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [ActionName("about")]
+        public ActionResult SomeMethodName() {
+            return View();
+        }
+        [NonAction]
+        public int Calc()
+        {
+            int sum = 0;
+            for (int i = 1; i < 10; i++)
+            {
+                sum = sum + i;
+            }
+            return sum;
+        }
+        [HttpPost]
+        public string VerbsTest() {
+            return "Hello";
+        }
+
+        public int displayCalc() {
+            return Calc();
+        }
+    }
+}
