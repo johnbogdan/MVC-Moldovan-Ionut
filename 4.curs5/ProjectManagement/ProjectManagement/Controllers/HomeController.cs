245e369103fd8cdc1d7d2d17ddabe7cd081812a1
johnbogdan <ionut_moldo94@yahoo.com>
2017-01-22 14:14:34 +0200

    Cursul 5  Project manager
    doar cursul 5

---
+++ 4.curs5/ProjectManagement/ProjectManagement/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using ProjectManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ProjectManagement.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Author = "Ionut";
+
+            var model = new AboutModel();
+            model.Message = "Hello user";
+            model.Author = "Ionut";
+            return View(model);
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+     
+    }
+}
